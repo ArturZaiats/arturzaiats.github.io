c274df6cb5f1624ac923aff24c33f6654e01ef3b
Artur <stayaz.artur@gmail.com>
2024-03-02 21:29:29 +0200

    added slick-slider

--- src/js/scrcipt.js
+++ src/js/scrcipt.js
@@ -1,1 +1,18 @@
-alert(1);
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="src/icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="src/icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                dots: true,
+                arrows: false,
+                }
+            }
+        ]
+      });
+});
+      
