a04bce26e5a1c3790cfbc957310ebd831c04bd12
Artur <stayaz.artur@gmail.com>
2024-03-07 10:45:30 +0200

    index.html goes to  src

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,11 @@ gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "."
+            baseDir: "src"
         }
     });
 
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
