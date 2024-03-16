3fe5cd882ea35815765427453a1aec85d1310818
Artur <stayaz.artur@gmail.com>
2024-03-16 19:37:59 +0200

    added animations

--- src/js/scrcipt.js
+++ src/js/scrcipt.js
@@ -125,10 +125,12 @@ $(document).ready(function(){
         }
     });
 
-    $("a[href^=']").clik(function(){
+    $("a[href=#up]").click(function(){
         const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
     });
+
+    new WOW().init();
 });
       
